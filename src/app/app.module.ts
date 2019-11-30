155fca655f4e04148e66872cdc078b930e19a1c6
Younes El Alami <shadoow.ma@gmail.com>
2019-11-30 23:35:00 +0800

    add firebase, authservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
+
+// Env
 import en from '@angular/common/locales/en';
+import { environment } from '../environments/environment';
+
+// Firebase
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 registerLocaleData(en);
 
@@ -25,7 +33,10 @@ registerLocaleData(en);
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCMy-8lzBiGh9jLLcCZI4_m56caeZtrcrg',
+    authDomain: 'job-hunt-fb252.firebaseapp.com',
+    databaseURL: 'https://job-hunt-fb252.firebaseio.com',
+    projectId: 'job-hunt-fb252',
+    storageBucket: 'job-hunt-fb252.appspot.com',
+    messagingSenderId: '816609935247',
+    appId: '1:816609935247:web:446ea2ac10c9e2329bd5fa',
+    measurementId: 'G-GBCX3HTYHW'
+  }
 };
 
 /*
