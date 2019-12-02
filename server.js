2a44e9dc73b538048daf060399991fe80480f948
Younes El Alami <shadoow.ma@gmail.com>
2019-12-03 07:58:46 +0800

    add gzip compression to express, add alt attribute to imgs

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const path = require('path');   
-
+const compression = require('compression')
 const app = express();   
 
+app.use(compression());
 app.use(express.static(__dirname + '/dist/job-ng'));
 
 app.get('/*', function(req,res) {  
