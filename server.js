d61450b036a5d20bfd5d0494eb8b2f54630b77a7
Younes El Alami <shadoow.ma@gmail.com>
2019-12-03 06:11:29 +0800

    fix bug in server.js routing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/job-ng'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname + '/job-ng/dist/index.html'));   
-});  
+    res.sendFile(path.join(__dirname + '/dist/job-ng/index.html'));   
+});
 
 app.listen(process.env.PORT || 8080);
